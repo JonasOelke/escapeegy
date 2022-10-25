32f7e5102d484b89f8ac4530a151902d10e24733
Jonas Oelke <Jonas.Oelke@haw-hamburg.de>
2022-10-25 15:47:48 +0200

    UI Test
    Imported the TextMesh Pro Package

---
+++ Assets/UI.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class UI : MonoBehaviour
+{
+    /* public CubeController cube;
+
+    private void OnEnable()
+    {
+        VisualElement root = GetComponent<UIDocument>().rootVisualElement;
+
+        Button buttonStart = root.Q<Button>("ButtonStart");
+        Button buttonStop = root.Q<Button>("ButtonStop");
+        Button buttonColor = root.Q<Button>("ButtonColor");
+
+        buttonStart.clicked += () => cube.StartRotate();
+        buttonStop.clicked += () => cube.StopRotate();
+        buttonColor.clicked += () => cube.ChangeColor();
+    } */
+}

9fa64ffd5b19b82f13713a89603a9c6d53dd06c5
Jonas Oelke <Jonas.Oelke@haw-hamburg.de>
2022-10-29 22:02:22 +0200

    added Inventory and chat screen for ui test scene

--- Assets/UI.cs
+++ Assets/UI.cs
@@ -6,18 +6,52 @@ using UnityEngine.UIElements;
 
 public class UI : MonoBehaviour
 {
-    /* public CubeController cube;
-
     private void OnEnable()
     {
         VisualElement root = GetComponent<UIDocument>().rootVisualElement;
 
-        Button buttonStart = root.Q<Button>("ButtonStart");
-        Button buttonStop = root.Q<Button>("ButtonStop");
-        Button buttonColor = root.Q<Button>("ButtonColor");
+        Button inventoryButton = root.Q<Button>("InventoryButton");
+        VisualElement inventoryContainer = root.Q<VisualElement>("InventoryContainer");
 
-        buttonStart.clicked += () => cube.StartRotate();
-        buttonStop.clicked += () => cube.StopRotate();
-        buttonColor.clicked += () => cube.ChangeColor();
-    } */
+        Button chatButton = root.Q<Button>("ChatButton");
+        VisualElement chatContainer = root.Q<VisualElement>("ChatContainer");
+
+        //Disable the inventory and chat containers, just in case someone forgot to do it in the editor
+        inventoryContainer.style.display = DisplayStyle.None;
+        chatContainer.style.display = DisplayStyle.None;
+
+        Button suggestion1 = root.Q<Button>("Suggestion1");
+        Button suggestion2 = root.Q<Button>("Suggestion2");
+        ScrollView chatMessagesContainer = root.Q<ScrollView>("ChatMessagesContainer");
+
+        inventoryButton.clicked += () =>
+        {
+            chatContainer.style.display = DisplayStyle.None;
+            inventoryContainer.style.display =
+                inventoryContainer.style.display == DisplayStyle.Flex
+                    ? DisplayStyle.None
+                    : DisplayStyle.Flex;
+        };
+
+        chatButton.clicked += () =>
+        {
+            chatContainer.style.display =
+                chatContainer.style.display == DisplayStyle.Flex
+                    ? DisplayStyle.None
+                    : DisplayStyle.Flex;
+            inventoryContainer.style.display = DisplayStyle.None;
+        };
+
+        suggestion1.clicked += () =>
+        {
+            VisualElement msgContainer = new VisualElement();
+            msgContainer.AddToClassList("chatMessageContainer");
+            Label msg = new Label("Hast du noch einen\nHinweis für mich?");
+            msg.AddToClassList("chatMessage");
+            msgContainer.Add(msg);
+
+            chatMessagesContainer.Add(msgContainer);
+            suggestion1.text = "nix";
+        };
+    }
 }

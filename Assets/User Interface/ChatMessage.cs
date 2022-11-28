f2ee1eeec5e5506564b6caa762d445e5f4b30204
Jonas Oelke <Jonas.Oelke@haw-hamburg.de>
2022-11-28 17:03:27 +0100

    Update ChatMessage.cs

--- Assets/User Interface/ChatMessage.cs
+++ Assets/User Interface/ChatMessage.cs
@@ -1,16 +1,30 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Reflection.Emit;
 using UnityEngine;
-using ChatResponse;
 
 public class ChatMessage : MonoBehaviour
 {
     int id;
     int[] nextIds;
-    bool sent=false;
-    string text;
-    bool available=false;
+    bool sent = false;
+    public string text;
+    bool available = false;
     ChatResponse[] responses;
     public GameObject photo;
-    
+
+    public ChatMessage(
+        int id,
+        int[] nextIds,
+        string text,
+        ChatResponse[] responses,
+        GameObject photo = null
+    )
+    {
+        this.id = id;
+        this.nextIds = nextIds;
+        this.text = text;
+        this.responses = responses;
+        this.photo = photo;
+    }
 }

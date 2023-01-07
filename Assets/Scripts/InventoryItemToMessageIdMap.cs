25289b21050180a5a5d191305abe6271587d4de3
josiela <joseffa.steuernagel@haw-hamburg.de>
2023-01-07 19:14:07 +0100

    Added Monas kommas

--- Assets/Scripts/InventoryItemToMessageIdMap.cs
+++ Assets/Scripts/InventoryItemToMessageIdMap.cs
@@ -11,8 +11,7 @@ public class InventoryItemToMessageIdMap
         {"MaggiesTagebucheintrag2", 10 },
         {"MaggiesTagebucheintrag3", 24 },
         {"MaggiesTagebucheintrag4", 34 },
-        {"ElliesBrief2", 34 },     
-<<<<<<< HEAD
+        {"ElliesBrief2", 34 },
         {"Verlaufsbericht1", 49 },
         {"Verlaufsbericht2", 49 },
         {"Verlaufsbericht3", 49 },
@@ -27,22 +26,6 @@ public class InventoryItemToMessageIdMap
         {"FietesBesuch", 56 },
         {"BettieUndMaggie", 57 },
         {"Nestbau", 58 },
-=======
-        {"Verlaufsbericht1", 49 }
-        {"Verlaufsbericht2", 49 }
-        {"Verlaufsbericht3", 49 }
-        {"Klinikarbeit", 53 }
-        {"Kliniksaal", 54 }
-        {"FietesBesuch", 56 }
-        {"Bildschnipsel1", 55 }
-        {"Bildschnipsel2", 55 }
-        {"Bildschnipsel3", 55 }
-        {"Bildschnipsel4", 55 }
-        {"Bildschnipsel5", 55 }
-        {"FietesBesuch", 56 }
-        {"BettieUndMaggie", 57 }
-        {"Nestbau", 58 }
->>>>>>> 9600c73 (Chat zuordnung mit bildern)
         { "DrogenWerbung", 60 }
         //Wenn ein Asset gesendet wurde, das nicht in dieser Liste ist, soll es als nachricht 62 gesendet werden
     };

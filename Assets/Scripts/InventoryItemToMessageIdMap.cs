9981bad97f69d791f21747d61fe8afe94609d8c1
Mona <mona.vonhein@web.de>
2023-01-07 18:08:18 +0100

    Chat zuordnung mit bildern --> commit reroll

--- Assets/Scripts/InventoryItemToMessageIdMap.cs
+++ Assets/Scripts/InventoryItemToMessageIdMap.cs
@@ -12,6 +12,7 @@ public class InventoryItemToMessageIdMap
         {"MaggiesTagebucheintrag3", 24 },
         {"MaggiesTagebucheintrag4", 34 },
         {"ElliesBrief2", 34 },     
+<<<<<<< HEAD
         {"Verlaufsbericht1", 49 },
         {"Verlaufsbericht2", 49 },
         {"Verlaufsbericht3", 49 },
@@ -26,6 +27,22 @@ public class InventoryItemToMessageIdMap
         {"FietesBesuch", 56 },
         {"BettieUndMaggie", 57 },
         {"Nestbau", 58 },
+=======
+        {"Verlaufsbericht1", 49 }
+        {"Verlaufsbericht2", 49 }
+        {"Verlaufsbericht3", 49 }
+        {"Klinikarbeit", 53 }
+        {"Kliniksaal", 54 }
+        {"FietesBesuch", 56 }
+        {"Bildschnipsel1", 55 }
+        {"Bildschnipsel2", 55 }
+        {"Bildschnipsel3", 55 }
+        {"Bildschnipsel4", 55 }
+        {"Bildschnipsel5", 55 }
+        {"FietesBesuch", 56 }
+        {"BettieUndMaggie", 57 }
+        {"Nestbau", 58 }
+>>>>>>> 9600c73 (Chat zuordnung mit bildern)
         { "DrogenWerbung", 60 }
         //Wenn ein Asset gesendet wurde, das nicht in dieser Liste ist, soll es als nachricht 62 gesendet werden
     };

b592c10727cdd35b500425c3871bad0ecc312596
Aodh <Francie.Roessler@web.de>
2023-01-07 18:29:13 +0100

    fixed somewhat or so...

--- Assets/Scripts/InventoryItemToMessageIdMap.cs
+++ Assets/Scripts/InventoryItemToMessageIdMap.cs
@@ -12,20 +12,20 @@ public class InventoryItemToMessageIdMap
         {"MaggiesTagebucheintrag3", 24 },
         {"MaggiesTagebucheintrag4", 34 },
         {"ElliesBrief2", 34 },     
-        {"Verlaufsbericht1", 49 }
-        {"Verlaufsbericht2", 49 }
-        {"Verlaufsbericht3", 49 }
-        {"Klinikarbeit", 53 }
-        {"Kliniksaal", 54 }
-        {"FietesBesuch", 56 }
-        {"Bildschnipsel1", 55 }
-        {"Bildschnipsel2", 55 }
-        {"Bildschnipsel3", 55 }
-        {"Bildschnipsel4", 55 }
-        {"Bildschnipsel5", 55 }
-        {"FietesBesuch", 56 }
-        {"BettieUndMaggie", 57 }
-        {"Nestbau", 58 }
+        {"Verlaufsbericht1", 49 },
+        {"Verlaufsbericht2", 49 },
+        {"Verlaufsbericht3", 49 },
+        {"Klinikarbeit", 53 },
+        {"Kliniksaal", 54 },
+        {"FietesBesuch", 56 },
+        {"Bildschnipsel1", 55 },
+        {"Bildschnipsel2", 55 },
+        {"Bildschnipsel3", 55 },
+        {"Bildschnipsel4", 55 },
+        {"Bildschnipsel5", 55 },
+        {"FietesBesuch", 56 },
+        {"BettieUndMaggie", 57 },
+        {"Nestbau", 58 },
         { "DrogenWerbung", 60 }
         //Wenn ein Asset gesendet wurde, das nicht in dieser Liste ist, soll es als nachricht 62 gesendet werden
     };
